bae6036fda1a9b6c5e7db26d6fd4764371f787f7
ivanovkirilg <ivanovkirilg@gmail.com>
2022-02-26 11:01:50 +0200

    init commit

---
+++ intro.h
@@ -0,0 +1,34 @@
+#ifndef C_OURSE_INTRO_H
+#define C_OURSE_INTRO_H
+
+#include <stdio.h>
+
+static int get_int(void)
+{
+    int input = 0;
+
+    if (scanf(" %d", &input) != 1)
+    {
+        printf("Enter a whole number: ");
+        while (getchar() != '\n');
+        input = get_int();
+    }
+
+    return input;
+}
+
+static float get_float(void)
+{
+    float input = 0;
+
+    if (scanf(" %f", &input) != 1)
+    {
+        printf("Enter a decimal number: ");
+        while (getchar() != '\n');
+        input = get_float();
+    }
+
+    return input;
+}
+
+#endif // C_OURSE_INTRO_H
